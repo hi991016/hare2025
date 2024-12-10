e0e996f76d9b0b151b8425fbcc7aa594dd0ae0ff
hi991016 <miureus@gmail.com>
2024-12-10 19:24:50 +0700

    coding top homepage done :fire:

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,30 @@
 const init = () => {
   // # app-height
   appHeight();
-  // # lenis
-  initLenis();
+  // # init loading
+  initLoading();
 };
+
+// ===== add event on multiple element =====
+const addEventOnElements = function (elements, eventType, callback) {
+  if (elements) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
+};
+
+// ===== lenis =====
+const lenis = new Lenis({
+  lerp: 0.05,
+  smoothWheel: true,
+});
+lenis.on("scroll", (e) => {});
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
 // ===== appheight =====
 const appHeight = () => {
@@ -18,18 +39,46 @@ const appHeight = () => {
 };
 window.addEventListener("resize", appHeight);
 
-// ===== lenis =====
-const initLenis = () => {
-  const lenis = new Lenis({
-    lerp: 0.05,
-    smoothWheel: true,
-  });
-  lenis.on("scroll", (e) => {});
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-  requestAnimationFrame(raf);
+// ===== swiper top =====
+const topSwiper = new Swiper("[data-top-swiper]", {
+  effect: "fade",
+  speed: 2000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  on: {
+    init: function (swiper) {
+      swiper.autoplay.stop();
+    },
+    slideChange: function (swiper) {
+      if (swiper.activeIndex == swiper.slides.length - 1) {
+        swiper.autoplay.stop();
+      }
+    },
+  },
+});
+
+// ===== init loading =====
+const [loading, loadingLogo] = [
+  document.querySelector("[data-loading]"),
+  document.querySelector("[data-loading-logo]"),
+];
+const initLoading = function () {
+  // ##
+  lenis.stop();
+  setTimeout(() => {
+    loadingLogo.classList.add("--fadein");
+  }, 800);
+  // ##
+  setTimeout(function () {
+    loading.classList.add("--done");
+    setTimeout(() => {
+      lenis.start();
+      topSwiper.autoplay.start();
+    }, 1000);
+  }, 2400);
 };
 
 // ===== lazy loading =====
